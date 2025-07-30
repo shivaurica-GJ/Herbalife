229c7d274628c772de7d3f8a8733a606790d4fb1
ShivAurica-GJ <133239865+mauryajatin45@users.noreply.github.com>
2025-07-30 10:36:20 +0530

    Remove unused React import from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Star, Users, Award, ArrowRight, CheckCircle } from "lucide-react";
 import { useLanguage } from "../contexts/LanguageContext";
